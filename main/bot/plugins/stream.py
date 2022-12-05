d5e200ce65cadb80a3dbf4496bf04b45f35f3d06
NesamaniX1 <77948100+NesamaniX1@users.noreply.github.com>
2022-12-05 16:52:48 +0530

    Update stream.py

--- main/bot/plugins/stream.py
+++ main/bot/plugins/stream.py
@@ -8,21 +8,22 @@ from main.vars import Var
 from pyrogram import filters, Client
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from main.utils.droplink import get_shortlink
 
-@StreamBot.on_message(
-    filters.private
-    & ~filters.user(Var.BANNED_USERS) & (
-        filters.document
-        | filters.video
-        | filters.audio
-        | filters.animation
-        | filters.voice
-        | filters.video_note
-        | filters.photo
-        | filters.sticker
-    ),
-    group=4,
-)
+# @StreamBot.on_message(
+#     filters.private
+#     & ~filters.user(Var.BANNED_USERS) & (
+#         filters.document
+#         | filters.video
+#         | filters.audio
+#         | filters.animation
+#         | filters.voice
+#         | filters.video_note
+#         | filters.photo
+#         | filters.sticker
+#     ),
+#     group=4,
+# )
 async def private_receive_handler(c: Client, m: Message):
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
@@ -40,7 +41,7 @@ async def private_receive_handler(c: Client, m: Message):
         await asyncio.sleep(e.x)
         await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Got Floodwait Of {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**User ID :** `{str(m.from_user.id)}`", disable_web_page_preview=True,)
 
-@StreamBot.on_message(filters.channel & ~filters.user(Var.BANNED_USERS) & (filters.document | filters.video) & ~filters.edited, group=-1)
+# @StreamBot.on_message(filters.channel & ~filters.user(Var.BANNED_USERS) & (filters.document | filters.video) & ~filters.edited, group=-1)
 async def channel_receive_handler(bot, broadcast: Message):
     if int(broadcast.chat.id) in Var.BANNED_CHANNELS:
         await bot.leave_chat(broadcast.chat.id)
@@ -56,12 +57,14 @@ async def channel_receive_handler(bot, broadcast: Message):
             # text=f"**Cʜᴀɴɴᴇʟ Nᴀᴍᴇ:** `{broadcast.chat.title}`\n**Cʜᴀɴɴᴇʟ ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** https://t.me/FxStreamBot?start=msgid_{str(log_msg.message_id)}",
             quote=True,            
         )
+        stream_link= await get_shortlink(stream_link)
         await bot.edit_message_reply_markup(
             chat_id=broadcast.chat.id,
             message_id=broadcast.message_id,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Download Link 📥", url=stream_link)]])
         )
+        
     except FloodWait as w:
         print(f"Sleeping for {str(w.x)}s")
         await asyncio.sleep(w.x)
@@ -73,7 +76,7 @@ async def channel_receive_handler(bot, broadcast: Message):
         print(f"Can't Edit Broadcast Message!\nEʀʀᴏʀ: {e}")
 
 # Feature is Dead no New Update for Stream Link on Group
-@StreamBot.on_message(filters.group & ~filters.user(Var.BANNED_USERS) & (filters.document | filters.video | filters.audio) & ~filters.edited, group=4)
+# @StreamBot.on_message(filters.group & ~filters.user(Var.BANNED_USERS) & (filters.document | filters.video | filters.audio) & ~filters.edited, group=4)
 async def private_receive_handler(c: Client, m: Message):
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
